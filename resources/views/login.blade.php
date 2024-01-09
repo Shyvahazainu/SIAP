52d65314ca794473da763ad51db6d5ef26b05ce3
azzalzilia <azzalziliasari@gmail.com>
2024-01-09 15:44:16 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="asset/css/login.css" />
+</head>
+<body>
+    <div class="judul-login">
+        <img src="asset/img/logo-kj.png" alt="Logo Kejaksaan">
+        <h1>Arsip Kejaksaan Tinggi Aceh <br>Bidang Tindak Pidana Khusus</h1>
+    </div>
+
+    <div class="container">
+        <div class="m-login">
+            <h2>Masuk</h2>
+            <h3>Masuk dan mulai lihat arsip</h3>
+
+            <input type="text" class="input-nip" id="nip" placeholder="NIP">
+            <input type="password" class="input-pass" id="password" placeholder="Password">
+
+        </div>
+    </div>
+</body>
+</html>
